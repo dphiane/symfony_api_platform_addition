884d6f4bc00d6f4be8c3b073aff1f8055de39030
Ubuntu <ubuntu@vps-b2fb0920.vps.ovh.net>
2024-07-02 13:29:37 +0000

    updated from vps

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     CoopTilleuls\ForgotPasswordBundle\CoopTilleulsForgotPasswordBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
